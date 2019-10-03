aa5ab545c3deb87e89098b90fe74aae4bcab4c2b
clonex10100 <clonex10100@gmail.com>
2019-10-03 06:12:35 -0400

    Fixed topic groups when scraping a specific group page

--- lib/page_manger.rb
+++ lib/page_manger.rb
@@ -5,15 +5,19 @@ class PageManager
     data = Scraper.scrape_page(url)
     #Set page_id in page data hash
     data[0][:page_id] = @@index
+    #Create page object
+    page = Page.create(data[0])
+
     #Set page_id in all topic data hashes
+    #Set group in all hashes if applicible
     data[1].each do |topic_hash|
+      if page.group
+        topic_hash[:group] = page.group
+      end
       topic_hash[:page_id] = @@index
     end
     @@index += 1;
     #Create topic objects
     Topic.create_from_array(data[1])
-    #Create page object
-    page = Page.create(data[0])
-    page
   end
 end
