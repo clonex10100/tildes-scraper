6695a7a9a1032f70b8b00d0da0b54aed8a0ae39b
clonex10100 <clonex10100@gmail.com>
2019-10-04 23:44:59 -0400

    Edited files to work with new lib structure

--- config/enviornment.rb
+++ config/enviornment.rb
@@ -2,6 +2,6 @@ require 'open-uri'
 require 'bundler'
 Bundler.require
 
-require_all 'lib/concerns'
-require_relative '../lib/topic/topic'
+require_all 'lib/tilde_scraper/concerns'
+require_relative '../lib/tilde_scraper/topic/topic'
 require_all 'lib'
